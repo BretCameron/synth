3e1ad2cc6d4f22e79cc40c944d60afa61a1d775a
Bret Cameron <bret@yulife.com>
2021-07-05 22:25:33 +0100

    Add arrayEquals util

--- index.js
+++ index.js
@@ -140,7 +140,22 @@ const playKey = (key) => {
   }
   pressedNotes.set(key, osc);
   pressedNotes.get(key).start();
+
+  // TODO - work out chords from notes
+  const notes = Array.from(pressedNotes).map(([key]) => keys[key].note);
+  console.log(notes);
 };
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
 
 const stopKey = (key) => {
   if (!keys[key]) {
